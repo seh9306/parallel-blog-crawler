db044d9aed8cdfa26caa8a04ddcbd3a6cd02603d
seh9306 <seh9306@gmail.com>
2020-12-29 01:08:08 +0900

    FlatLaf 적용

--- src/main/java/team/dev/blog/config/AppConfig.java
+++ src/main/java/team/dev/blog/config/AppConfig.java
@@ -1,16 +1,33 @@
 package team.dev.blog.config;
 
+import com.formdev.flatlaf.FlatLightLaf;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.swing.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+@Slf4j
 @Configuration
-public class AppConfig {
+@RequiredArgsConstructor
+public class AppConfig implements InitializingBean {
 
     @Bean
     public ConcurrentLinkedQueue<String> concurrentLinkedQueue() {
         return new ConcurrentLinkedQueue<>();
     }
 
+    @Override
+    public void afterPropertiesSet() {
+        try {
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            JDialog.setDefaultLookAndFeelDecorated(true);
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception ex) {
+            log.error("Failed to initialize LaF");
+        }
+    }
 }
