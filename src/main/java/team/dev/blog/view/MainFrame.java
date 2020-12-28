b8b5c286b495e742450c79c842930d889e6690c6
seh9306 <seh9306@gmail.com>
2020-12-29 01:11:40 +0900

    파일 아이콘과 로고 추가

--- src/main/java/team/dev/blog/view/MainFrame.java
+++ src/main/java/team/dev/blog/view/MainFrame.java
@@ -1,14 +1,30 @@
 package team.dev.blog.view;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import team.dev.blog.view.panel.MainPanel;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Component
 public class MainFrame extends JFrame {
-    public MainFrame(ConcurrentLinkedQueue<String> queue) {
+    public static MainFrame thisFrame = null;
+
+    public MainFrame(ConcurrentLinkedQueue<String> queue) throws IOException {
+        super("NDEV 크롤러");
+        thisFrame = this;
+        setResizable(false);
+        setSize(250, 150);
+
+        ClassPathResource classPathResource = new ClassPathResource("ico/search_cat.png");
+        setIconImage(ImageIO.read(classPathResource.getURL()));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(200,200);
         setContentPane(new MainPanel(queue));
